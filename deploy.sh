c38075d5fbbead43126a76c5ca658828d3e0bdd6
Sean <sy170107@gmail.com>
2020-02-09 00:58:08 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/sh
 tar zcvf blog.tgz public/
-scp -P $SSH_PORT -v blog.tgz "${SSH_USER}@${SSH_ADDR}:~/"
+scp -P $SSH_PORT blog.tgz "${SSH_USER}@${SSH_ADDR}:~/"
 ssh -p $SSH_PORT "${SSH_USER}@${SSH_ADDR}" < ./deploy-blog.sh
