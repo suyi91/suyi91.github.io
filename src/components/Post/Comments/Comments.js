59f7d70b9357cfb289260b209a1183dfac767239
Sean <sy170107@gmail.com>
2020-02-22 20:56:36 +0800

    test gitalk

--- src/components/Post/Comments/Comments.js
+++ src/components/Post/Comments/Comments.js
@@ -1,27 +1,44 @@
 // @flow strict
 import React from 'react';
-import ReactDisqusComments from 'react-disqus-comments';
-import { useSiteMetadata } from '../../../hooks';
+// import ReactDisqusComments from 'react-disqus-comments';
+import GitalkComponent from 'gitalk/dist/gitalk-component';
+// import { useSiteMetadata } from '../../../hooks';
 
-type Props = {
-  postTitle: string,
-  postSlug: string
-};
+// type Props = {
+//   postTitle: string,
+//   postSlug: string
+// };
 
-const Comments = ({ postTitle, postSlug }: Props) => {
-  const { url, disqusShortname } = useSiteMetadata();
+// const Comments = ({ postTitle, postSlug }: Props) => {
+//   const { url, disqusShortname } = useSiteMetadata();
 
-  if (!disqusShortname) {
-    return null;
-  }
+//   if (!disqusShortname) {
+//     return null;
+//   }
+
+//   return (
+//     <ReactDisqusComments
+//       shortname={disqusShortname}
+//       identifier={postTitle}
+//       title={postTitle}
+//       url={url + postSlug}
+//     />
+//   );
+// };
+
+const Comments = () => {
+  const options = {
+    clientID: '17f31ff09b4f57a72479',
+    clientSecret: 'a9a76b6b4e21452413ea830535fa9304f50ba11a',
+    repo: 'suyi.xyz.comments',
+    owner: 'suyi91',
+    admin: ['suyi91'],
+    id: window.location.pathname,
+    distractionFreeMode: true
+  };
 
   return (
-    <ReactDisqusComments
-      shortname={disqusShortname}
-      identifier={postTitle}
-      title={postTitle}
-      url={url + postSlug}
-    />
+    <GitalkComponent options={options} />
   );
 };
 
