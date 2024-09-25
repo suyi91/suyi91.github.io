b44ee9923b00c11c0a852f2d816aa558c2656981
Sean <sy170107@gmail.com>
2024-09-25 23:33:04 +0800

    update

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -9,16 +9,10 @@ const Footer = () => {
       © {year} Built with <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby</a>
     </span>
   );
-  const footerRight = (
-    <a className={styles['footer__right']} href="https://beian.miit.gov.cn/" target="_blank">
-      苏ICP备18013443号
-    </a>
-  );
   return (
     <footer className={styles['footer']}>
       {footerLeft}
       <span className={styles['footer__divider']}>❤️</span>
-      {footerRight}
     </footer>
   );
 };
