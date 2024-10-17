46094dbc8d716d6c1e73efa10908ae183eee388e
Sean <sy170107@gmail.com>
2024-10-17 21:53:08 +0800

    update project settings

---
+++ deploy.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { execSync, exec } = require('child_process');
+
+const GH_TOKEN = process.env.GH_TOKEN;
+
+if (!GH_TOKEN) {
+  console.error('Missing GH_TOKEN');
+  process.exit(1);
+}
+
+execSync(`gh-pages -d dist --cname suyi.xyz -b master -r \"https://${GH_TOKEN}@github.com/suyi91/suyi91.github.io.git\" -m \"Auto-generated commit\"`)
+
