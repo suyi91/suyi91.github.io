4dace9b1c3f5bee091c0367c681b34d76aada136
Sean <sy170107@gmail.com>
2026-04-14 22:37:30 +0800

    fix: make formatDate, PostTitle, PostViewTitle lang-aware

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,18 +1,14 @@
 import dayjs from "dayjs";
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 import utc from "dayjs/plugin/utc";
-import {t} from '../i18n/utils';
-import {config} from '../consts';
+import { useTranslations, type Lang } from '../i18n/utils';
 
-dayjs.locale(config.lang);
 dayjs.extend(advancedFormat)
 dayjs.extend(utc);
 
-export function formatDate(date,dateType='post.dateFormat') {
-  if (date) {
-    const dateFormat = t(dateType) || "YYYY-MM-DD";
-    return dayjs(date).utc().format(dateFormat);
-  } else {
-    return ''
-  }
+export function formatDate(date: Date | string | undefined, lang: Lang = 'en', dateType = 'post.dateFormat'): string {
+  if (!date) return ''
+  const t = useTranslations(lang)
+  const dateFormat = t(dateType) || 'YYYY-MM-DD'
+  return dayjs(date).utc().format(dateFormat)
 }
